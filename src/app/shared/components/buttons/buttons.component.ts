8abd1df4c621ec3556b1778519dcc2abbd5314da
n.runeev <nruneev@mail.ru>
2023-02-11 21:13:34 +0300

    Add button component

---
+++ src/app/shared/components/buttons/buttons.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, HostBinding, Input} from '@angular/core';
+
+import {Icon} from "../../enum/icon";
+
+
+@Component({
+  selector: 'app-buttons',
+  templateUrl: './buttons.component.html',
+  styleUrls: ['./buttons.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class ButtonsComponent {
+  @Input() icon: Icon = Icon.none;
+  @Input() name: string = '';
+  @Input() isBackground: boolean = false;
+
+  @HostBinding('class.g-flex-j-b') get isSpaceBetween() {
+    return this.icon !== Icon.none && !!this.name.length;
+  };
+  @HostBinding('class.buttons-background') get isButtonBackground() {
+    return this.isBackground;
+  };
+}
---
+++ src/app/shared/enum/icon.ts
@@ -0,0 +1,8 @@
+export enum Icon {
+  close = 'close',
+  edit = 'edit',
+  trash = 'trash',
+  view = 'view',
+  plus = 'plus',
+  none = 'none'
+}
