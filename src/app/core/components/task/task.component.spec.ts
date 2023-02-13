5b0d8fa3ce413a5fc6f3f8806ff6d4efce72b1b4
nruneev <nruneev@mail.ru>
2023-02-13 04:27:54 +0300

    Add Test

--- src/app/core/components/task/task.component.spec.ts
+++ src/app/core/components/task/task.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, tick} from '@angular/core/testing';
 
-import { TaskComponent } from './task.component';
+import {TaskComponent} from './task.component';
+import {MatCardModule} from "@angular/material/card";
+
+import {StatusTask} from "../../enum/status-task";
+import {ButtonsModule} from "../../../shared/components/buttons/buttons.module";
+
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
@@ -8,16 +13,39 @@ describe('TaskComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskComponent ]
+      declarations: [ TaskComponent ],
+      imports: [MatCardModule, ButtonsModule]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(TaskComponent);
     component = fixture.componentInstance;
+    component.task = {
+      title: 'Example',
+      description: "Example description",
+      time: 30,
+      status: StatusTask.new,
+      dataUpdate: new Date(),
+      id: 1
+    }
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should correct update status', () => {
+    expect(component.task.status).toEqual(StatusTask.new);
+
+    component.task.status = StatusTask.process;
+    fixture.detectChanges();
+
+    expect(component.task.status).toEqual(StatusTask.process);
+
+    component.task.status = StatusTask.done;
+    fixture.detectChanges();
+
+    expect(component.task.status).toEqual(StatusTask.done);
+  })
 });
